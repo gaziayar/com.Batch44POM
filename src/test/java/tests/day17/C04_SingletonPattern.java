af21af3b5a2c8f0b25389b22dc4a1e781c5f734e
Gazi Ayar <gazitacik@gmail.com>
2022-02-03 23:25:31 +0100

    ve

---
+++ src/test/java/tests/day17/C04_SingletonPattern.java
@@ -0,0 +1,13 @@
+package tests.day17;
+
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class C04_SingletonPattern {
+    @Test
+    public void test01(){
+       // Driver driver=new Driver();
+
+
+    }
+}
